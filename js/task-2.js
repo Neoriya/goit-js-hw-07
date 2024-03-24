aa090c67c824307436e6b33bb60ef13715e29b9d
Neoriya <alena.neori@gmail.com>
2024-03-24 20:02:35 +0200

    tasks only JS

--- js/task-2.js
+++ js/task-2.js
@@ -12,18 +12,64 @@ const images = [
     alt: 'Group of Horses Running',
   },
   {
-    url:
-      'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     alt: 'Elephant Beside on Baby Elephant',
   },
   {
-    url:
-      'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     alt: 'Blue Geeen and Orange Parrot',
   },
   {
-    url:
-      'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     alt: 'Zebras on Zebra',
   },
 ];
+
+// Напиши скрипт для створення галереї зображень на основі масиву даних. HTML містить список ul.gallery.
+// <ul class="gallery"></ul>;
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>.
+// Ти можеш створити й додати HTML-елементи, використовуючи document.createElement() і elem.append() або шаблонні рядки і elem.insertAdjacentHTML().
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами через CSS класи.
+
+// На що буде звертати увагу ментор при перевірці:
+// Створена й додана в DOM галерея із трьох зображень
+// Галерея додана у список ul.gallery і являє собою 3 елементи <li>, в які вкладені елементи <img>
+// Для створення елементів <img> використані дані з масиву об’єктів images
+// Усі елементи галереї додані в DOM за одну операцію додавання
+// Є мінімальне оформлення галереї флексбоксами через CSS класи
+
+const container = document.querySelector('.gallery');
+
+function createGallery(picturesArr) {
+  return picturesArr
+    .map(
+      ({ url, alt }) =>
+        `
+    <li class="card">
+    <div class="image-box" width = '360' height = '300'>
+      <img src="${url}" alt="${alt}" class="picture" width = '360' height = '300'>
+      </div>
+    </li>
+  `
+    )
+    .join('');
+}
+
+container.style.display = 'flex';
+container.style.flexWrap = 'wrap';
+container.style.gap = '25px';
+container.insertAdjacentHTML('beforeend', createGallery(images));
+
+// images.forEach(img => {
+//   const listItem = document.createElement('li');
+//   const imgElement = document.createElement('img');
+
+//   imgElement.src = img.url;
+//   imgElement.alt = img.alt;
+
+//   listItem.appendChild(imgElement);
+//   container.appendChild(listItem);
+
+//   console.log(container);
+// });
--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,28 @@
+// Напиши скрипт, який змінює колір фону елемента <body> через інлайн-стиль по кліку на button.change-color і задає це значення кольору текстовим вмістом для span.color.
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor().
+
+// Зверни увагу, що функція getRandomHexColor() повертає колір у hex-форматі, в той час як колір фону на <body> буде у форматі rgb. Це нормально й не потребує якихось правок.
+
+// На що буде звертати увагу ментор при перевірці:
+
+// Фон на <body> задається тільки після кліку на button.change-color
+// При кожному кліку на елемент button.change-color фон <body> зафарбовується новим рандомним кольором
+// На <body> і span.color значення одного й того самого кольору
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+const bodyColor = document.querySelector('body');
+const numberBodyColor = document.querySelector('.color');
+const buttonClass = document
+  .querySelector('.change-color')
+  .addEventListener('click', handlerClick);
+
+function handlerClick(event) {
+  const randomColor = getRandomHexColor(event);
+  bodyColor.style.backgroundColor = randomColor;
+  numberBodyColor.textContent = `- ${randomColor}`;
 }
